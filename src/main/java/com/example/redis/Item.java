fd1b1549493e105f3dc330a82aff46b6fe3a6640
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-08-19 22:27:18 +0900

    Feat : Item 클래스 생성
    Redis에 저장할 Item 클래스 생성

---
+++ src/main/java/com/example/redis/Item.java
@@ -0,0 +1,24 @@
+package com.example.redis;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@RedisHash("item")
+public class Item implements Serializable {
+    @Id
+    private Long id;
+    private String name;
+    private String description;
+    private Integer price;
+}
